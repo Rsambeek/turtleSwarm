49868d901e3db42968520fecb66ee38ad327061d
Rick <rickvansambeek@gmail.com>
2021-01-25 15:23:01 +0100

    update

--- autoSetup.lua
+++ autoSetup.lua
@@ -1,3 +1,7 @@
 -- pastebin get QPvcpwU2 autoSetup.lua
+fs.delete("ComputerControl/")
+fs.delete("Programs/")
+fs.delete("startup.lua")
+fs.delete("Toolbox.lua")
 shell.run("pastebin get W5ZkVYSi gitget")
 shell.run("gitget Rsambeek turtleSwarm development")
